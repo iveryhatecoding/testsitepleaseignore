95b03822f1d9e91928d922b19e60ff86f05e4999
iveryhatecoding <167974378+iveryhatecoding@users.noreply.github.com>
2025-05-19 14:14:03 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+fetch('https://<your-render-backend-url>/api/clients')
+  .then(res => res.json())
+  .then(data => {
+    const container = document.getElementById('clients');
+    data.forEach(c => {
+      const div = document.createElement('div');
+      div.className = 'card';
+      div.innerHTML = `<strong>${c.first_name} ${c.last_name}</strong><br>
+        Risk Assessment Due: ${c.next_periodic_risk_assessment}`;
+      container.appendChild(div);
+    });
+  });
